0f0b6bec86152cfb8d1877545ca5e6a262fcbec2
AloeFlawinGulenywa <aloeflawingulenywa98@gmail.com>
2023-09-20 18:53:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+let symbol = "*"; "||";
+
+function saturdayFun(txt = "roller-skate") {
+    return "This Saturday, I want to " + txt + "!";
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+console.log (mondayWork());
+console.log (mondayWork("work from home"))
+
+
+let wrapAdjective = function(AFG="*") {
+    return function (adjective = "special") {
+        return `You are ${AFG}${adjective}${AFG}!`
+    } 
+               
+}
